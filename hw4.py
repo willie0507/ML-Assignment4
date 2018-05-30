3026915591c34dcf9b7a33e349c09b5e362f4717
willie <willie0507@gmail.com>
2018-05-30 22:26:18 +0800

    data preprocess

--- hw4.py
+++ hw4.py
@@ -1,17 +1,21 @@
-import pandas as pd
+import sys
+# import pandas as pd
+if (sys.argv[1] == '--train'):
+    bias = 3
+    fp = open(r'upload_full2/training_data/Training freq 1D, OW 16, PW 2.csv', 'r')
+elif (sys.argv[1] == '--test'):
+    bias = 4
+    fp = open(r'upload_full2/test_feature/Verification freq 1D, OW 16, PW 2_feature.csv', 'r')
+for i in range(4):
+    line = fp.readline()
 
-fp = open(r'full1_upload/full1_upload/training_data/Training freq 1D, OW 1, PW 1.csv', 'r')
-line = fp.readline()
-line = fp.readline()
-line = fp.readline()
-line = fp.readline()
 data = []
 label= []
 while line:
     tmp = []
     for i in range(1, 85):
         idx = line.find(',')
-        if(i < 3):
+        if(i < bias):
             line = line[idx+1:]
             continue
         if(i == 84):
@@ -28,12 +32,20 @@ while line:
     del tmp
 fp.close()
 
-with open('train.txt', 'a') as fp:
+if(bias == 3):
+    output = 'train_1D_OW16_PW2.txt'
+else:
+    output = 'test_1D_OW16_PW2.txt'  
+with open(output, 'w') as fp:
+    pass
+with open(output, 'a') as fp:
     for i, v in enumerate(data):
         if(label[i] == 'True'):
             fp.write(str(1) + ' ')
         elif(label[i] == 'False'):
             fp.write(str(0) + ' ')
+        elif(bias == 4):
+            fp.write(str(9) + ' ')
         else:
             continue
         for j in v:
